019e624d38376fe739f6665cbffaa3cdbe15bcc5
Juliette <211356692+jfran01@users.noreply.github.com>
2026-06-09 16:09:19 +0100

    check for winning conditions

--- noughts_and_crosses.rb
+++ noughts_and_crosses.rb
@@ -1,6 +1,6 @@
 class Game 
   @@players = []
-  attr_accessor :board
+  attr_accessor :board, :current_player
   
   def initialize 
     @player_one = Player.new("noughts", 1)
@@ -23,6 +23,7 @@ class Game
     chosen_cell = gets().chomp.to_i - 1
     @current_player.assign_cell(@board, chosen_cell)
     self.render_board()
+    self.check_for_winner
     self.switch_players
   end
 
@@ -41,6 +42,27 @@ class Game
       board_interface.push(row)
     end
     puts board_interface
+  end
+
+  def check_for_winner
+    self.board.each_slice(3) do |row|
+      if row.all?
+        if row.uniq.length == 1
+          self.declare_winner
+        end
+      end
+    end
+    self.board[0..2].each_with_index do |cell, index|
+      if cell && cell == self.board[index + 3] && cell == self.board[index + 6]
+        self.declare_winner
+      end
+    end
+  end
+
+  def declare_winner 
+    @winner = self.current_player
+    puts "Hurrah! We have a winner! Well done Player #{self.current_player.id}"
+    exit
   end
 
   def switch_players
