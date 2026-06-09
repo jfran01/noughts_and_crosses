f8445a80f61864a6a5b4518cd83c56611e45486e
Juliette <211356692+jfran01@users.noreply.github.com>
2026-06-09 14:36:28 +0100

    set up Game class

--- noughts_and_crosses.rb
+++ noughts_and_crosses.rb
@@ -1,3 +1,15 @@
+class Game 
+  @@players = []
+  
+  def initialize 
+    @player_one = Player.new("noughts", 1)
+    @player_two = Player.new("crosses", 2)
+    @current_player = @player_one
+    @board = Array.new(9)
+    @@players.push(@player_one, @player_two)
+  end
+end
+
 class Player
   attr_accessor :symbol
   attr_reader :id
